1e81edfd1bda2d89c0e8eef69f6925a605cc7f51
Hongyi Li <iygnohil@gmail.com>
2018-06-25 03:39:00 +0800

    Regrouped tests

--- test.js
+++ test.js
@@ -245,87 +245,90 @@ describe("Definition", () => {
     })
   });
 
-});
+  describe("Signal", () => {
 
-describe("Signal Definition", () => {
+    it("initializes definition without error", () => {
+      new Signal(define(() => {}));
+    })
 
-  it("initializes definition without error", () => {
-    new Signal(define(() => {}));
-  })
+    it("reads definition without error", () => {
+      new Signal(define(() => {}))();
+    })
 
-  it("reads definition without error", () => {
-    new Signal(define(() => {}))();
-  })
+    it("reads definition return value", () => {
+      const signal = new Signal(define(() => 2))
+      assert.equal(signal(), 2);
+    })
 
-  it("reads definition return value", () => {
-    const signal = new Signal(define(() => 2))
-    assert.equal(signal(), 2);
-  })
-
-});
-
-describe("Reactor Definition", () => {
-
-  it("sets definition without error", () => {
-    const reactor = new Reactor();
-    reactor.foo = define(() => "bar");
-  })
-
-  it("gets definition return value", () => {
-    const reactor = new Reactor();
-    reactor.foo = define(() => "bar");
-    assert.equal(reactor.foo, "bar");
-  })
-
-  it("can override definition", () => {
-    const reactor = new Reactor();
-    reactor.foo = define(() => "bar");
-    assert.equal(reactor.foo, "bar");
-    reactor.foo = 2;
-    assert.equal(reactor.foo, 2);
-  })
-
-  it("can set definition as non-writable", () => {
-    const reactor = new Reactor();
-    Object.defineProperty(reactor, "foo", {
-      value: define(() => "bar"),
-      writable: false
-    });
-    assert.equal(reactor.foo, "bar");
-    reactor.foo = 2;
-    assert.equal(reactor.foo, "bar");
-  })
-
-  it("sets definition as enumerable by default", () => {
-    const reactor = new Reactor();
-    reactor.foo = define(() => "bar");
-    assert.equal(Object.keys(reactor).length, 1);
-  })
-
-  it("can set definition as non-enumerable", () => {
-    const reactor = new Reactor();
-    Object.defineProperty(reactor, "foo", {
-      value: define(() => "bar"),
-      enumerable: false
-    });
-    assert.equal(Object.keys(reactor).length, 0);
   });
 
-  // Temporarily disabled test since this seems fundamentally impossible
-  // due to implementation where you cannot reconfigure the descriptor in a trap
-  // if configurable is set to false. 
-  // File bug here https://bugs.chromium.org/p/v8/issues/detail?id=7884
-  // it("can set definition as non-configurable", () => {
-  //   const reactor = new Reactor();
-  //   Object.defineProperty(reactor, "foo", {
-  //     value: define(() => "bar"),
-  //     configurable: false
-  //   });
-  //   delete reactor.foo;
-  //   assert.equal(reactor.foo, "bar")
-  // });
+  describe("Reactor", () => {
+
+    it("sets definition without error", () => {
+      const reactor = new Reactor();
+      reactor.foo = define(() => "bar");
+    })
+
+    it("gets definition return value", () => {
+      const reactor = new Reactor();
+      reactor.foo = define(() => "bar");
+      assert.equal(reactor.foo, "bar");
+    })
+
+    it("can override definition", () => {
+      const reactor = new Reactor();
+      reactor.foo = define(() => "bar");
+      assert.equal(reactor.foo, "bar");
+      reactor.foo = 2;
+      assert.equal(reactor.foo, 2);
+    })
+
+    it("can set definition as non-writable", () => {
+      const reactor = new Reactor();
+      Object.defineProperty(reactor, "foo", {
+        value: define(() => "bar"),
+        writable: false
+      });
+      assert.equal(reactor.foo, "bar");
+      reactor.foo = 2;
+      assert.equal(reactor.foo, "bar");
+    })
+
+    it("sets definition as enumerable by default", () => {
+      const reactor = new Reactor();
+      reactor.foo = define(() => "bar");
+      assert.equal(Object.keys(reactor).length, 1);
+    })
+
+    it("can set definition as non-enumerable", () => {
+      const reactor = new Reactor();
+      Object.defineProperty(reactor, "foo", {
+        value: define(() => "bar"),
+        enumerable: false
+      });
+      assert.equal(Object.keys(reactor).length, 0);
+    });
+
+    // Temporarily disabled test since this seems fundamentally impossible
+    // due to implementation where you cannot reconfigure the descriptor in a trap
+    // if configurable is set to false. 
+    // File bug here https://bugs.chromium.org/p/v8/issues/detail?id=7884
+    // it("can set definition as non-configurable", () => {
+    //   const reactor = new Reactor();
+    //   Object.defineProperty(reactor, "foo", {
+    //     value: define(() => "bar"),
+    //     configurable: false
+    //   });
+    //   delete reactor.foo;
+    //   assert.equal(reactor.foo, "bar")
+    // });
+
+  });
 
 });
+
+
+
 
 describe("Observer", () => {
 
@@ -384,227 +387,305 @@ describe("Observer", () => {
       message: "Cannot create observer with a non-function"
     });
   });
-  
-});
 
-describe("Observering", () => {
-  it("should trigger on initialization", () => {
-    let tracker = 0;
-    const observer = new Observer(() => tracker = 1);
-    assert.equal(tracker, 1);
-  });
-  it("should trigger on Signal dependency write", () => {
-    let tracker = 0;
-    const signal = new Signal(1);
-    const observer = new Observer(() => (tracker = signal()));
-    signal(2);
-    assert.equal(tracker, 2);
-  });
-  it("should trigger once per Signal dependency write", () => {
-    let counter = 0;
-    const signal = new Signal("foo");
-    const observer = new Observer(() => {
-      signal();
-      counter += 1;
+  describe("Triggering", () => {
+
+    it("triggers once on initialization", () => {
+      let counter = 0;
+      const observer = new Observer(() => counter += 1);
+      assert.equal(counter, 1);
     });
-    assert.equal(counter, 1);
-    signal("bar");
-    assert.equal(counter, 2);
-    signal("foo");
-    assert.equal(counter, 3);
-  });
-  it("should trigger on Reactor dependency write", () => {
-    let tracker = 0;
-    const reactor = new Reactor();
-    reactor.foo = 1;
-    const observer = new Observer(() => (tracker = reactor.foo));
-    reactor.foo = 2;
-    assert.equal(tracker, 2);
-  });
-  it("should trigger once per Reactor dependency write", () => {
-    let counter = 0;
-    const reactor = new Reactor();
-    reactor.foo = "foo";
-    const observer = new Observer(() => {
-      counter += 1;
-      reactor.foo;
+
+    it("triggers once on Signal dependency write", () => {
+      let counter = 0;
+      let tracker;
+      const signal = new Signal("foo");
+      const observer = new Observer(() => {
+        counter += 1;
+        tracker = signal();
+      });
+      assert.equal(counter, 1);
+      assert.equal(tracker, "foo");
+      signal("bar");
+      assert.equal(counter, 2);
+      assert.equal(tracker, "bar");
     });
-    assert.equal(counter, 1);
-    reactor.foo = "bar";
-    assert.equal(counter, 2);
-    reactor.foo = "foo";
-    assert.equal(counter, 3);
-  });
-  it("should trigger on Reactor dependency write", () => {
-    let tracker = 0;
-    const reactor = new Reactor();
-    const observer = new Observer(() => (tracker = reactor.foo));
-    reactor.foo = define(() => "foo");
-    assert.equal(tracker, "foo");
-  });
-  it("should trigger on nested Reactor dependency write", () => {
-    let tracker = 0;
-    const reactor = new Reactor({
-      foo: {
-        bar: "baz"
-      }
+
+
+    it("triggers once on Signal dependency defined write", () => {
+      let counter = 0;
+      let tracker;
+      const signal = new Signal("foo");
+      const observer = new Observer(() => {
+        counter += 1;
+        tracker = signal();
+      });
+      assert.equal(counter, 1);
+      assert.equal(tracker, "foo");
+      signal(define(() => "bar"));
+      assert.equal(counter, 2);
+      assert.equal(tracker, "bar");
     });
-    const observer = new Observer(() => (tracker = reactor.foo.bar));
-    assert.equal(tracker, "baz");
-    reactor.foo.bar = "moo";
-    assert.equal(tracker, "moo");
-  });
-  it("should trigger on defineProperty", () => {
-    let tracker = 0;
-    const reactor = new Reactor({
-      foo: "bar"
+
+    it("triggers once on Reactor dependency write", () => {
+      let counter = 0;
+      let tracker;
+      const reactor = new Reactor({
+        foo: "bar"
+      });
+      const observer = new Observer(() => {
+        counter += 1;
+        tracker = reactor.foo;
+      });
+      assert.equal(counter, 1);
+      assert.equal(tracker, "bar");
+      reactor.foo = "mux"
+      assert.equal(counter, 2);
+      assert.equal(tracker, "mux");
     });
-    const observer = new Observer(() => (tracker = reactor.foo));
-    Object.defineProperty(reactor, "foo", {
-      get() { return "baz"; }
-    })
-    assert.equal(tracker, "baz");
-  });
-  it("should trigger on deleteProperty", () => {
-    let tracker = 0;
-    const reactor = new Reactor({
-      foo: "bar"
+
+    it("triggers once on Reactor dependency defined write", () => {
+      let counter = 0;
+      let tracker;
+      const reactor = new Reactor();
+      const observer = new Observer(() => {
+        counter += 1;
+        tracker = reactor.foo;
+      });
+      assert.equal(counter, 1);
+      assert.equal(tracker, undefined);
+      reactor.foo = define(() => "mux");
+      assert.equal(counter, 2);
+      assert.equal(tracker, "mux");
     });
-    const observer = new Observer(() => (tracker = reactor.foo));
-    delete reactor.foo;
-    assert.equal(tracker, undefined);
-  });
-  it("should be able to stop", () => {
-    let tracker = null;
-    let counter = 0;
-    const signal = new Signal("foo");
-    const observer = new Observer(() => {
-      counter += 1;
-      tracker = signal()
+
+    it("triggers once on nested Reactor dependency write", () => {
+      let counter = 0;
+      let tracker;
+      const reactor = new Reactor({
+        foo: {
+          bar: "baz"
+        }
+      });
+      const observer = new Observer(() => {
+        counter += 1;
+        tracker = reactor.foo.bar;
+      });
+      assert.equal(counter, 1);
+      assert.equal(tracker, "baz");
+      reactor.foo.bar = "moo";
+      assert.equal(counter, 2);
+      assert.equal(tracker, "moo");
     });
-    assert.equal(tracker, "foo");
-    assert.equal(counter, 1);
-    signal("bar");
-    assert.equal(tracker, "bar");
-    assert.equal(counter, 2);
-    observer.stop();
-    signal("moo");
-    assert.equal(tracker, "bar");
-    assert.equal(counter, 2);
-  });
-  it("should be able to start after stopping", () => {
-    let tracker = null;
-    let counter = 0;
-    const signal = new Signal("foo");
-    const observer = new Observer(() => {
-      counter += 1;
-      tracker = signal()
+
+    it("triggers on defineProperty", () => {
+      let tracker;
+      const reactor = new Reactor({
+        foo: "bar"
+      });
+      const observer = new Observer(() => (tracker = reactor.foo));
+      assert.equal(tracker, "bar");
+      Object.defineProperty(reactor, "foo", {
+        get() { return "baz"; }
+      });
+      assert.equal(tracker, "baz");
     });
-    assert.equal(tracker, "foo");
-    assert.equal(counter, 1);
-    observer.stop();
-    signal("moo");
-    assert.equal(tracker, "foo");
-    assert.equal(counter, 1);
-    observer.start();
-    assert.equal(tracker, "moo");
-    assert.equal(counter, 2);
-  });
-  it("should start idempotently", () => {
-    let tracker = null;
-    let counter = 0;
-    const signal = new Signal("foo");
-    const observer = new Observer(() => {
-      counter += 1;
-      tracker = signal()
+
+    it("trigger on deleteProperty", () => {
+      let tracker;
+      const reactor = new Reactor({
+        foo: "bar"
+      });
+      const observer = new Observer(() => (tracker = reactor.foo));
+      assert.equal(tracker, "bar");
+      delete reactor.foo;
+      assert.equal(tracker, undefined);
     });
-    assert.equal(tracker, "foo");
-    assert.equal(counter, 1);
-    observer.stop();
-    signal("moo");
-    assert.equal(tracker, "foo");
-    assert.equal(counter, 1);
-    observer.start();
-    assert.equal(tracker, "moo");
-    assert.equal(counter, 2);
-    observer.start();
-    assert.equal(tracker, "moo");
-    assert.equal(counter, 2);
+
+    it("triggers on array update methods", () => {
+      let counter = 0;
+      let tracker;
+      const reactor = new Reactor([]);
+      const observer = new Observer(() => {
+        counter += 1;
+        tracker = reactor[0];
+      });
+      assert.equal(counter, 1);
+      assert.equal(tracker, undefined);
+      reactor.push("foo");
+      assert.equal(counter, 2);
+      assert.equal(tracker, "foo");
+      reactor.unshift("bar");
+      assert.equal(counter, 3);
+      assert.equal(tracker, "bar");
+    });
+
+    it("triggers correctly on nested observer definitions", () => {
+      const outerSignal = new Signal("foo");
+      const innerSignal = new Signal("bar");
+      let outerCounter = 0;
+      let innerCounter = 0;
+      let outerTracker;
+      let innerTracker;
+      let outerObserver;
+      let innerObserver;
+      outerObserver = new Observer(() => {
+        outerCounter += 1;
+        outerTracker = outerSignal();
+        if (innerObserver) innerObserver.stop();
+        innerObserver = new Observer(() => {
+          innerCounter += 1;
+          innerTracker = innerSignal();
+        });
+      });
+      assert.equal(outerCounter, 1);
+      assert.equal(outerTracker, "foo");
+      assert.equal(innerCounter, 1);
+      assert.equal(innerTracker, "bar");
+      innerSignal("baz");
+      assert.equal(outerCounter, 1);
+      assert.equal(outerTracker, "foo");
+      assert.equal(innerCounter, 2);
+      assert.equal(innerTracker, "baz");
+      outerSignal("moo");
+      assert.equal(outerCounter, 2);
+      assert.equal(outerTracker, "moo");
+      assert.equal(innerCounter, 3);
+      assert.equal(innerTracker, "baz");
+    });
+
   });
-  it("should not infinite loop", () => {
-    let tracker = null;
-    let counter = 0;
-    const signal = new Signal("foo");
-    assert.throws(
-      () => {
+
+  describe("Start Stop", () => {
+
+    it("can stop observing", () => {
+      let counter = 0;
+      let tracker;
+      const signal = new Signal("foo");
+      const observer = new Observer(() => {
+        counter += 1;
+        tracker = signal()
+      });
+      assert.equal(counter, 1);
+      assert.equal(tracker, "foo");
+      signal("bar");
+      assert.equal(counter, 2);
+      assert.equal(tracker, "bar");
+      observer.stop();
+      signal("moo");
+      assert.equal(counter, 2);
+      assert.equal(tracker, "bar");
+    });
+
+    it("can start after stopping", () => {
+      let counter = 0;
+      let tracker = null;
+      const signal = new Signal("foo");
+      const observer = new Observer(() => {
+        counter += 1;
+        tracker = signal();
+      });
+      assert.equal(counter, 1);
+      assert.equal(tracker, "foo");
+      observer.stop();
+      signal("moo");
+      assert.equal(counter, 1);
+      assert.equal(tracker, "foo");
+      observer.start();
+      assert.equal(counter, 2);
+      assert.equal(tracker, "moo");
+    });
+
+    it("has no effect with repeated starts", () => {
+      let counter = 0;
+      let tracker = null;
+      const signal = new Signal("foo");
+      const observer = new Observer(() => {
+        counter += 1;
+        tracker = signal();
+      });
+      assert.equal(counter, 1);
+      assert.equal(tracker, "foo");
+      observer.stop();
+      signal("moo");
+      assert.equal(counter, 1);
+      assert.equal(tracker, "foo");
+      observer.start();
+      assert.equal(counter, 2);
+      assert.equal(tracker, "moo");
+      observer.start();
+      assert.equal(counter, 2);
+      assert.equal(tracker, "moo");
+    });
+
+  });
+
+  describe("Error Handling", () => {
+
+    it("throws an error on an Observer loop", () => {
+      let counter = 0;
+      let tracker;
+      const signal = new Signal("foo");
+      assert.throws(() => {
         const observer = new Observer(() => {
           counter += 1;
           tracker = signal();
           if (counter < 100) signal("bar");
         });
-      },
-      { 
+      }, {
         name: "LoopError",
         message: "observer attempted to activate itself while already executing"
-      }
-    )
-    assert.equal(tracker, "foo");
-    assert.equal(counter, 1);
+      });
+      assert.equal(counter, 1);
+      assert.equal(tracker, "foo");
+    });
+
+    it("throws an error on a Signal write if there is an Observer error", () => {
+      const signal = new Signal(1);
+      const observer = new Observer(() => {
+        if (signal() > 1) throw new Error("dummy error");
+      });
+      assert.throws(() => signal(2), {
+        name: "Error",
+        message: "dummy error"
+      });
+    });
+
+    it("throws a CompoundError if there are multiple Observer errors", () => {
+      const signal = new Signal(1);
+      const observer1 = new Observer(() => {
+        if (signal() > 1) throw new Error("dummy error 1");
+      });
+      const observer2 = new Observer(() => {
+        if (signal() > 1) throw new Error("dummy error 2");
+      });
+      assert.throws(() => signal(2), {
+        name: "CompoundError"
+      });
+    });
+
   });
-  it("should throw observer error from signal update", () => {
-    const signal = new Signal(1);
-    const observer = new Observer(() => {
-      if (signal() > 1) throw new Error("dummy error");
+
+  describe("Misc", () => {
+
+    it("respects receiver context for prototype inheritors", () => {
+      const reactor = new Reactor();
+      reactor.foo = "bar";
+      Object.defineProperty(reactor, "getFoo", {
+        get() {
+          return this.foo;
+        }
+      })
+      assert.equal(reactor.getFoo, "bar");
+      reactor.foo = "quu";
+      assert.equal(reactor.getFoo, "quu");
+      const inheritor = Object.create(reactor);
+      assert.equal(inheritor.foo, "quu");
+      assert.equal(inheritor.getFoo, "quu");
+      inheritor.foo = "mux"
+      assert.equal(inheritor.getFoo, "mux");
     });
-    assert.throws(() => signal(2), {
-      name: "Error",
-      message: "dummy error"
-    });
+
   });
-  it("should throw compound error if multiple observer errors", () => {
-    const signal = new Signal(1);
-    const observer1 = new Observer(() => {
-      if (signal() > 1) throw new Error("dummy error 1");
-    });
-    const observer2 = new Observer(() => {
-      if (signal() > 1) throw new Error("dummy error 2");
-    });
-    assert.throws(() => signal(2), {
-      name: "CompoundError"
-    });
-  });
-  it("should observe array methods", () => {
-    let counter = 0;
-    let tracker;
-    const reactor = new Reactor([]);
-    const observer = new Observer(() => {
-      counter += 1;
-      tracker = reactor[0];
-    });
-    assert.equal(counter, 1);
-    assert.equal(tracker, undefined);
-    reactor.push("foo");
-    assert.equal(counter, 2);
-    assert.equal(tracker, "foo");
-    reactor.unshift("bar");
-    assert.equal(counter, 3);
-    assert.equal(tracker, "bar");
-  });
-  it("should respect receiver context for prototype inheritors", () => {
-    const reactor = new Reactor();
-    reactor.foo = "bar";
-    Object.defineProperty(reactor, "getFoo", {
-      get() {
-        return this.foo;
-      }
-    })
-    assert.equal(reactor.getFoo, "bar");
-    reactor.foo = "quu";
-    assert.equal(reactor.getFoo, "quu");
-    const inheritor = Object.create(reactor);
-    assert.equal(inheritor.foo, "quu");
-    assert.equal(inheritor.getFoo, "quu");
-    inheritor.foo = "mux"
-    assert.equal(inheritor.getFoo, "mux");
-  });
-})
+
+});
