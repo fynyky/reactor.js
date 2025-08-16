9770c6a9443c83fd3f6af830f691bd2dc10dc0bc
fynyky <iygnohil@gmail.com>
2025-08-16 18:33:10 +0800

    Cleaned up error handling tests

--- test/test.js
+++ test/test.js
@@ -1706,6 +1706,65 @@ describe('Hiding', () => {
   })
 })
 
+describe('Error Handling', () => {
+  it('should throw an error on a write if there is an Observer throws an error', () => {
+    const reactor = new Reactor({ value: 'foo' })
+    new Observer(() => {
+      if (reactor.value > 1) throw new Error('dummy error')
+    })()
+    assert.throws(() => (reactor.value = 2), {
+      name: 'Error',
+      message: 'dummy error'
+    })
+  })
+
+  it('should throw a CompoundError if there are multiple Observer errors', () => {
+    const reactor = new Reactor({ value: 1 })
+    new Observer(() => {
+      if (reactor.value > 1) throw new Error('dummy error 1')
+    })()
+    new Observer(() => {
+      if (reactor.value > 1) throw new Error('dummy error 2')
+    })()
+    assert.throws(() => (reactor.value = 2), {
+      name: 'CompoundError'
+    })
+  })
+
+  it('should throw a flattened CompoundError with chained observers', () => {
+    const reactor = new Reactor({
+      foo: 1
+    })
+    // Successful passthrough to create subsequent compound errors
+    new Observer(() => {
+      reactor.passthrough = reactor.foo
+    })()
+    // Initial error failures to create an initial compound error
+    new Observer(() => {
+      if (reactor.foo > 1) throw new Error('primary error 1')
+    })()
+    new Observer(() => {
+      if (reactor.foo > 1) throw new Error('primary error 2')
+    })()
+    // Chain off reactor.passthrough to create a subsequent compound error
+    new Observer(() => {
+      if (reactor.passthrough > 1) throw new Error('secondary error 1')
+    })()
+    new Observer(() => {
+      if (reactor.passthrough > 1) throw new Error('secondary error 2')
+    })()
+    // Setting foo to 2 should trigger the primary error 1 and 2 and trigger the passthrough
+    // The passthrough triggers secondary error 1 and 2 as well which get merged into a compound error
+    // This compound error should then be merged with primary error 1 and 2
+    // Instead of nesting it flattens to a single compound error with 4 causes
+    assert.throws(() => (reactor.foo = 2), (error) => {
+      assert.strictEqual(error.name, 'CompoundError')
+      assert.strictEqual(error.cause.length, 4)
+      return true
+    })
+  })
+})
+
 describe.skip('Misc', () => {
   it('should get triggered by multiple array operations in sequence', () => {
     let runCount = 0
@@ -1932,62 +1991,6 @@ describe.skip('Misc', () => {
       assert.equal(secondCounter, 2)
       assert.equal(firstTracker, 'foo')
       assert.equal(secondTracker, 'baz')
-    })
-  })
-
-  describe('Error Handling', () => {
-    it('should throw an error on a write if there is an Observer error', () => {
-      const reactor = new Reactor({ value: 'foo' })
-      new Observer(() => {
-        if (reactor.value > 1) throw new Error('dummy error')
-      })()
-      assert.throws(() => (reactor.value = 2), {
-        name: 'Error',
-        message: 'dummy error'
-      })
-    })
-
-    it('should throw a CompoundError if there are multiple Observer errors', () => {
-      const reactor = new Reactor({ value: 1 })
-      new Observer(() => {
-        if (reactor.value > 1) throw new Error('dummy error 1')
-      })()
-      new Observer(() => {
-        if (reactor.value > 1) throw new Error('dummy error 2')
-      })()
-      assert.throws(() => (reactor.value = 2), {
-        name: 'CompoundError'
-      })
-    })
-
-    it('should throw a flattened compound error with chained observers', () => {
-      const reactor = new Reactor({
-        foo: 'Bar'
-      })
-      // Successful passthrough to create subsequent compound errors
-      new Observer(() => {
-        reactor.passthrough = reactor.foo
-      })()
-      assert.equal(reactor.passthrough, 'Bar')
-      // Initial error failures to create an initial compound error
-      new Observer(() => {
-        if (reactor.foo === 'error') throw new Error('BIG ERROR 1')
-      })()
-      new Observer(() => {
-        if (reactor.foo === 'error') throw new Error('BIG ERROR 2')
-      })()
-      // Chain off reactor.passthrough to create a subsequent compound error
-      new Observer(() => {
-        if (reactor.passthrough === 'error') throw new Error('small error 1')
-      })()
-      new Observer(() => {
-        if (reactor.passthrough === 'error') throw new Error('small error 2')
-      })()
-      assert.throws(() => (reactor.foo = 'error'), (error) => {
-        assert.equal(error.name, 'CompoundError')
-        assert.equal(error.cause.length, 4)
-        return true
-      })
     })
   })
 
