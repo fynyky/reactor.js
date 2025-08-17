a80328a74e731d48da34a00d4ec080463c71a568
fynyky <iygnohil@gmail.com>
2025-08-18 00:03:05 +0800

    Added test for reactor write batching

--- test/test.js
+++ test/test.js
@@ -1855,48 +1855,18 @@ describe('Batching', () => {
     assert.strictEqual(observerC.value, 'barAbarB')
   })
 
-    // foo -> a -> c
-  // foo -> b -> c
-  // Auto batching for signals should trigger c once
-  // Even if signals are batched, some reactor writes are compound operations
-  // So need to batch at this level as well
-  it.skip('should automatically batch Reactor writes', () => {
-    const signal = new Signal('foo')
-    let runCountA = 0
-    let runCountB = 0
-    let runCountC = 0
-    const observerA = new Observer(() => {
-      runCountA += 1
-      return signal() + 'A'
-    })
-    observerA()
-    const observerB = new Observer(() => {
-      runCountB += 1
-      return signal() + 'B'
-    })
-    observerB()
-    const observerC = new Observer(() => {
-      runCountC += 1
-      return observerA.value + observerB.value
-    })
-    observerC()
-    assert.strictEqual(runCountA, 1)
-    assert.strictEqual(runCountB, 1)
-    assert.strictEqual(runCountC, 1)
-    assert.strictEqual(observerA.value, 'fooA')
-    assert.strictEqual(observerB.value, 'fooB')
-    assert.strictEqual(observerC.value, 'fooAfooB')
-    signal('bar')
-    assert.strictEqual(runCountA, 2)
-    assert.strictEqual(runCountB, 2)
-    // If not auto batched this would be 3
-    // As observerC would be trigger from both A and B
-    assert.strictEqual(runCountC, 2)
-    assert.strictEqual(observerA.value, 'barA')
-    assert.strictEqual(observerB.value, 'barB')
-    assert.strictEqual(observerC.value, 'barAbarB')
+  it('should automatically batch Reactor writes', () => {
+    let runCount = 0
+    const reactor = new Reactor(['foo', 'bar'])
+    new Observer(() => {
+      runCount += 1
+      return JSON.stringify(reactor)
+    })()
+    assert.strictEqual(runCount, 1)
+    // Call pop because it's a compound operation
+    reactor.pop()
+    assert.strictEqual(runCount, 2)
   })
-
 })
 
 describe('Hiding', () => {
